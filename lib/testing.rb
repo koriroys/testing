2fd7e3c9d0e1420dcfb76c62ecfdb9f009d31a4a
Kori Roys <kori@koriroys.com>
2015-09-23 20:03:56 +0000

    tests to make pass

--- lib/testing.rb
+++ lib/testing.rb
@@ -1,5 +1,78 @@
 require "testing/version"
 
+
+
 module Testing
-  # Your code goes here...
+  # Fork this gist, then fill in the code with your answers.
+
+  # you teach an advanced mathematics class at the local university.
+  # Your class is very difficult, so all but three students have quit.
+  # The three students who remain are:
+  # jim, 22 years old
+  # caroline, age 21 years old
+  # jasmine, the 17 year old young genius
+
+  # to keep track of your students, you create a Student class:
+
+  class Student
+#     attr_accessor :name, :age
+
+    def initialize(name:, age:)
+       @name = name
+       @age = age
+    end
+
+    attr_reader :name
+    attr_reader :age
+
+  end
+  
+  class Display
+    attr_reader :students
+    
+    def initialize(students)
+      @students = students
+    end
+
+    def names
+      names = []
+      @students.each do |student|
+        names << student.name.capitalize
+      end
+      names.join(", ")
+    end
+
+#     def average_age
+#       # fill in this method with the code to print out the average age of the class
+#       # hint: try to use a different method than 'each' from the Enumerable class
+#       # hint: (or maybe a combination of methods)
+#     end
+
+#     def student_info(student_name)
+#       # hint: you'll need to find the right student in the list of students,
+#       # then fetch that student's age as well.
+#       # EXPECTED OUTPUT, given a student_name of "jim":
+#       # Jim is 22 years old
+#     end
+  end
 end
+
+#     def name
+#       @name
+#     end
+# student.name
+
+    # attr_writer :name
+#     def name=(value)
+#       @name = value
+#     end
+# student.name=("jeff")
+
+# attr_accessor :name
+#     def name
+#       @name
+#     end
+
+#     def name=(value)
+#       @name = value
+#     end
--- test/testing_test.rb
+++ test/testing_test.rb
@@ -6,6 +6,69 @@ class TestingTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
+
+describe Testing do
+  it "has a student class" do
+    Testing::Student.must_be_instance_of Class
+  end
+
+  describe Testing::Student do
+    it "has a name" do
+      student = Testing::Student.new(name: "brian", age: 3)
+      student.name.must_equal("brian")
+    end
+
+    it "can set a name" do
+      student = Testing::Student.new(name: "jeff", age: 3)
+      student.name.must_equal("jeff")
+    end
+
+    it "can set an age" do
+      student = Testing::Student.new(name: "doesn't matter", age: 27)
+      student.age.must_equal(27)
+    end
+
+    describe "students" do
+      Student = Testing::Student
+      before do
+        @students = [
+          Student.new(name: 'jim', age: 22),
+          Student.new(name: 'caroline', age: 21),
+          Student.new(name: 'jasmine', age: 17)
+        ]
+      end
+
+      Display = Testing::Display
+
+      describe Display do
+        before do
+          @display = Display.new(@students)
+        end
+        it "has a list of all the students" do
+          @display.students.must_equal(@students)
+        end
+
+        it "has shows the names of all the students" do
+          @display.names.must_equal("Jim, Caroline, Jasmine")
+        end
+        
+        it "has shows the ages of all the students" do
+          @display.ages.must_equal("22, 21, 17")
+        end
+        
+        it "has shows average age of the class" do
+          @display.average_age.must_equal(20)
+        end
+        
+        it "shows info for a specific student" do
+          @display.student_info("jim").must_equal("Jim is 22 years old")
+        end
+      end
+    end
+  end
+end
+
+# bundler gem testing
